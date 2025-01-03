3e3ad09cd4f0a7a8a340b6a2c0d436cb322c3dab
lootabox <tupelibox@gmail.com>
2025-01-03 18:53:12 +0200

    make regen/firemaster talents scale speed of regen

--- data/Scripts/CONTENT/UltimateStranger/RegenMechanic.d
+++ data/Scripts/CONTENT/UltimateStranger/RegenMechanic.d
@@ -100,34 +100,71 @@ func void RegenMechanic_Init() {
 
     // For life
     address = oCNpc__Regenerate__Life_CallChangeAttribute_Start;
-    ASM_Open(24); // 17 + retn + 1
+    ASM_Open(56); // 49 + retn + 1
     // ----- Add jump from engine function to new code -----
         MEM_WriteByte(address + 0, ASMINT_OP_jmp);
         MEM_WriteInt (address + 1, ASMINT_CurrRun-address-5); // relative address
         MEM_WriteByte(address + 5, ASMINT_OP_nop);
 
-    /*  push -1
-        0x6a        0x00    */
-        ASM_1(106); ASM_1(-1);
-    /*  push ATR_REGENERATEHP
-        0x6a        0x00    */
-        ASM_1(106); ASM_1(ATR_REGENERATEHP);
-    /*  mov ecx, esi
-        0x8b        0xce    */
-        ASM_1(139); ASM_1(206);
-    /*  call    ?ChangeAttribute@oCNpc@@QAEXHH@Z    */
-        ASM_1(ASMINT_OP_call);  ASM_4(oCNpc__ChangeAttribute - ASM_Here() - 4);
+    // Get regen amount from talent
+    /*  mov edx, esi+TALENTS_ARRAY_OFFSET
+        0x8b        0x96        568h */
+        ASM_1(139); ASM_1(150); ASM_4(1384);
+    /*  mov edx, edx+TALENT_INDEX_OFFSET
+        0x8b        0x92        4*NPC_TALENT_REGENERATE */
+        ASM_1(139); ASM_1(146); ASM_4(4*NPC_TALENT_REGENERATE);
+    /*  mov edx, edx+TALENT_SKILL_OFFSET
+        0x8b        0x92        28h */
+        ASM_1(139); ASM_1(146); ASM_4(40);
 
-    /*  push 1
-        0x6a        0x00    */
-        ASM_1(106); ASM_1(1);
+    // Ensure regen amount does not exceed remaining regen count
+    /*  mov ecx, esi+ATTRIBUTES_ARRAY_OFFSET+ATR_REGENERATEHP_OFFSET
+        0x8b        0x8e        1B8h+4*ATR_REGENERATEHP */
+        ASM_1(139); ASM_1(142); ASM_4(440+4*ATR_REGENERATEHP);
+    /*  cmp edx, ecx
+        0x3b        0xd1 */
+        ASM_1(59);  ASM_1(209);
+    /*  jle .skipEnsure
+        0x7e        0x02 */
+        ASM_1(126); ASM_1(2);
+    /*  mov edx, ecx
+        0x8b    0xd1 */
+        ASM_1(139); ASM_1(209);
+    // skipEnsure:
+
+    // Add to hitpoints attribute
+    /*  push edx
+        0x52 */
+        ASM_1(82);
+    /*  push edx
+        0x52 */
+        ASM_1(82);
     /*  push ATR_HITPOINTS
-        0x6a        0x00    */
+        0x6a        ATR_HITPOINTS */
         ASM_1(106); ASM_1(ATR_HITPOINTS);
     /*  mov ecx, esi
-        0x8b        0xce    */
+        0x8b        0xce */
         ASM_1(139); ASM_1(206);
-    /*  ChangeAttribute call is in the original code after return.  */
+    /*  call    ?ChangeAttribute@oCNpc@@QAEXHH@Z */
+        ASM_1(ASMINT_OP_call);  ASM_4(oCNpc__ChangeAttribute - ASM_Here() - 4);
+
+    // Reduce from regen attribute
+    /*  pop edx
+        0x5a */
+        ASM_1(90);
+    /*  neg edx
+        0xf7        0xda */
+        ASM_1(247); ASM_1(218);
+    /*  push edx
+        0x52 */
+        ASM_1(82);
+    /*  push ATR_REGENERATEHP
+        0x6a        ATR_REGENERATEHP */
+        ASM_1(106); ASM_1(ATR_REGENERATEHP);
+    /*  mov ecx, esi
+        0x8b        0xce */
+        ASM_1(139); ASM_1(206);
+    /*  ChangeAttribute call is in the original code after return. */
 
     // ----- Return to engine function -----
         ASM_1(ASMINT_OP_pushIm);
@@ -138,34 +175,71 @@ func void RegenMechanic_Init() {
 
     // For mana
     address = oCNpc__Regenerate__Mana_CallChangeAttribute_Start;
-    ASM_Open(24); // 17 + retn + 1
+    ASM_Open(56); // 49 + retn + 1
     // ----- Add jump from engine function to new code -----
         MEM_WriteByte(address + 0, ASMINT_OP_jmp);
         MEM_WriteInt (address + 1, ASMINT_CurrRun-address-5); // relative address
         MEM_WriteByte(address + 5, ASMINT_OP_nop);
 
-    /*  push -1
-        0x6a        0x00    */
-        ASM_1(106); ASM_1(-1);
-    /*  push ATR_REGENERATEMANA
-        0x6a        0x00    */
-        ASM_1(106); ASM_1(ATR_REGENERATEMANA);
-    /*  mov ecx, esi
-        0x8b        0xce    */
-        ASM_1(139); ASM_1(206);
-    /*  call    ?ChangeAttribute@oCNpc@@QAEXHH@Z    */
-        ASM_1(ASMINT_OP_call);  ASM_4(oCNpc__ChangeAttribute - ASM_Here() - 4);
+    // Get regen amount from talent
+    /*  mov edx, esi+TALENTS_ARRAY_OFFSET
+        0x8b        0x96        568h */
+        ASM_1(139); ASM_1(150); ASM_4(1384);
+    /*  mov edx, edx+TALENT_INDEX_OFFSET
+        0x8b        0x92        4*NPC_TALENT_2H */
+        ASM_1(139); ASM_1(146); ASM_4(4*NPC_TALENT_2H);
+    /*  mov edx, edx+TALENT_SKILL_OFFSET
+        0x8b        0x92        28h */
+        ASM_1(139); ASM_1(146); ASM_4(40);
 
-    /*  push 1
-        0x6a        0x00    */
-        ASM_1(106); ASM_1(1);
+    // Ensure regen amount does not exceed remaining regen count
+    /*  mov ecx, esi+ATTRIBUTES_ARRAY_OFFSET+ATR_REGENERATEMANA_OFFSET
+        0x8b        0x8e        1B8h+4*ATR_REGENERATEMANA */
+        ASM_1(139); ASM_1(142); ASM_4(440+4*ATR_REGENERATEMANA);
+    /*  cmp edx, ecx
+        0x3b        0xd1 */
+        ASM_1(59);  ASM_1(209);
+    /*  jle .skipEnsure
+        0x7e        0x02 */
+        ASM_1(126); ASM_1(2);
+    /*  mov edx, ecx
+        0x8b    0xd1 */
+        ASM_1(139); ASM_1(209);
+    // skipEnsure:
+
+    // Add to hitpoints attribute
+    /*  push edx
+        0x52 */
+        ASM_1(82);
+    /*  push edx
+        0x52 */
+        ASM_1(82);
     /*  push ATR_MANA
-        0x6a        0x00    */
+        0x6a        ATR_MANA */
         ASM_1(106); ASM_1(ATR_MANA);
     /*  mov ecx, esi
-        0x8b        0xce    */
+        0x8b        0xce */
         ASM_1(139); ASM_1(206);
-    /*  ChangeAttribute call is in the original code after return.  */
+    /*  call    ?ChangeAttribute@oCNpc@@QAEXHH@Z */
+        ASM_1(ASMINT_OP_call);  ASM_4(oCNpc__ChangeAttribute - ASM_Here() - 4);
+
+    // Reduce from regen attribute
+    /*  pop edx
+        0x5a */
+        ASM_1(90);
+    /*  neg edx
+        0xf7        0xda */
+        ASM_1(247); ASM_1(218);
+    /*  push edx
+        0x52 */
+        ASM_1(82);
+    /*  push ATR_REGENERATEMANA
+        0x6a        ATR_REGENERATEMANA */
+        ASM_1(106); ASM_1(ATR_REGENERATEMANA);
+    /*  mov ecx, esi
+        0x8b        0xce */
+        ASM_1(139); ASM_1(206);
+    /*  ChangeAttribute call is in the original code after return. */
 
     // ----- Return to engine function -----
         ASM_1(ASMINT_OP_pushIm);
